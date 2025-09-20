54cf778cc15dd7be7ab87b2569730aac9d447715
Tran Vu Hoang (K18 HCM) <hoangtvse180682@fpt.edu.vn>
2025-09-20 11:33:07 +0700

    Upload Files Lên Remote

---
+++ Frontend/SWP391_2ndLand/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="project-name">Secondhand EV & Battery Trading Platform</h1>
+    </div>
+  )
+}
+
+export default App
