480b235dd0d43f0f57f820a2e759afcf41f2b32a
SatohTofu <saitohtedofu1982@gmail.com>
2025-10-12 21:47:38 +0700

    Database Seeding (Initializer)
    Hoàn thiện phần Database Seeding giúp lượng lớn dữ liệu để test.
    Đọc kỹ file Database_Seeding_README để biết rõ hơn cơ chế hoạt động và cấu trúc.

--- Backend-SWP391_2ndLand/src/main/java/project/swp/spring/sebt_platform/model/PostRequestEntity.java
+++ Backend-SWP391_2ndLand/src/main/java/project/swp/spring/sebt_platform/model/PostRequestEntity.java
@@ -1,11 +1,24 @@
 package project.swp.spring.sebt_platform.model;
 
-import jakarta.persistence.*;
-import org.hibernate.annotations.CreationTimestamp;
-import project.swp.spring.sebt_platform.model.enums.ApprovalStatus;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import project.swp.spring.sebt_platform.model.enums.ApprovalStatus;
 
 
 @Entity
@@ -19,7 +32,7 @@ public class PostRequestEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "listing_id", nullable = false)
     private ListingEntity listing;
 
